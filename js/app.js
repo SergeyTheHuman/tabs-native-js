aa021e207da7bd81006ef1745c039aad9e06b0b6
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-29 15:22:43 +0300

    created tabs

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@ import { isWebp } from './components/isWebp.js'
 
 isWebp()
 
+const $tabs = document.querySelector('.tabs')
+
+$tabs.addEventListener('click', (e) => {
+	if (e.target.classList.contains('tabs__button')) {
+		const btn = e.target
+		const tabPath = btn.dataset.tabsPath
+		const chosenBtn = $tabs.querySelector(`[data-tabs-path=${tabPath}]`)
+		const chosenItem = $tabs.querySelector(`[data-tabs-target=${tabPath}]`)
+		const currentBtn = $tabs.querySelector('.tabs__button--active')
+		const currentItem = $tabs.querySelector('.tabs__item--active')
+
+		changeTab(tabPath)
+		function changeTab(path) {
+			currentBtn.classList.remove('tabs__button--active')
+			currentItem.classList.remove('tabs__item--active')
+
+			chosenBtn.classList.add('tabs__button--active')
+			chosenItem.classList.add('tabs__item--active')
+		}
+	}
+})
